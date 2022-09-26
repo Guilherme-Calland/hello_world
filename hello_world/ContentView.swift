9bbfff1e5e61d751fdb85e16db7ed9928e504f86
Guilherme-Calland <gui.crb0@gmail.com>
2022-09-26 15:50:33 -0300

    Initial Commit

---
+++ hello_world/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello_world
+//
+//  Created by Guilherme Calland Rosa Borba on 26/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hello_world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello_world
+//
+//  Created by Guilherme Calland Rosa Borba on 26/09/22.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
